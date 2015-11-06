9a23f2710a6238bb3ee515a42b6c8c446a18b7fb
DBelvedere <davidjbelvedere@gmail.com>
2015-11-06 12:56:54 -0500

    Updated MVO.R File to be a function and return weights for each frontier point. Also included MVO_test.R to show how to use another file as a function.

--- MVO.R
+++ MVO.R
@@ -3,52 +3,25 @@ require(quantmod)
 require(corpcor)
 
 #if you don't have one of the above packages, use install.packages("packagename")
+MVO <- function(ticker, mylist, wmax, nports, shorts, rf){
 
-#read stock symbols
-ticker <- read.csv(file="XIU.csv",header=FALSE,sep=",",colClasses = c(V1="character"))$V1
+  for(i in 1:length(mylist)){
+    mylist[[i]] <- mylist[[i]][index(mylist[[1]])]
+  }
 
-from = as.Date('2013-10-01')
-to = Sys.Date()
-
-#download data from yahoo finance
-mylist <- lapply(ticker, function(x){
-  try(getSymbols(x, src = 'yahoo', from = from, to = to, auto.assign = FALSE))
-})
-
-#ensure all stocks are on the same time index
-for(i in 1:length(mylist)){
-  mylist[[i]] <- mylist[[i]][index(mylist[[1]])]
-}
-
-r<-na.remove(do.call(merge,lapply(lapply(mylist,Ad),annualReturn))) #extract adjusted close daily returns
+  r<-na.remove(do.call(merge,lapply(lapply(mylist,Ad),annualReturn))) #extract adjusted close daily returns
 
 
-averet = vector()
-#calculate geometric mean and store in averet
-for (i in 1:length(ticker)){
-  averet[i] <- prod((1+r[1:length(r[,1]),i]))^(1/length(r[,1]))-1
-}
-names(averet)<-ticker
-rcov <- cov(r) #calculate covariance matrix
-tgtret <- 0.05 #target return
-
-folio <- portfolio.optim(x = r, pm = tgtret, covmat = make.positive.definite(rcov), shorts = FALSE, 
-                           reslow = rep(0,length(ticker)), reshigh = rep(0.5,length(ticker)), riskless = TRUE, 
-                           rf = 0.01)
-names(folio$pw)<-ticker
-folio$pw <- round(100*folio$pw)/100
-print(folio$pw[folio$pw!=0])
-
-#calculate effecient frontiers
-wmax <- 1
-nports <- 50
-shorts <- TRUE
-
-eff<-effFrontier(matrix(averet,nrow=1),make.positive.definite(rcov),nports,shorts,wmax)
-plot(eff$vol,eff$ret,type='l',xlim = as.vector(c(min(eff$vol),max(eff$vol))),ylim = as.vector(c(min(eff$ret),max(eff$ret))))
-
-effFrontier = function (averet, rcov, nports, shorts, wmax)
-{
+  averet = vector()
+  #calculate geometric mean and store in averet
+  for (i in 1:length(ticker)){
+    averet[i] <- prod((1+r[1:length(r[,1]),i]))^(1/length(r[,1]))-1
+  }
+  
+  averet <- matrix(averet, nrow=1)
+  rcov <- make.positive.definite(cov(r))
+  
+  
   mxret = max(abs(averet))
   mnret = -mxret
   n.assets = ncol(averet)
@@ -62,16 +35,22 @@ effFrontier = function (averet, rcov, nports, shorts, wmax)
   min.rets = seq(mnret, mxret, len = nports)
   vol = rep(NA, nports)
   ret = rep(NA, nports)
+  wts = list()
   for (k in 1:nports)
   {
     port.sol = NULL
     try(port.sol <- portfolio.optim(x=averet, pm=min.rets[k], covmat=rcov,
-                                    reshigh=reshigh, reslow=reslow,shorts=shorts),silent=T)
+                                    reshigh=reshigh, reslow=reslow,shorts=shorts,riskless = TRUE, rf = rf),silent=T)
     if ( !is.null(port.sol) )
     {
-      vol[k] = sqrt(as.vector(port.sol$pw %*% rcov %*% port.sol$pw))
-      ret[k] = averet %*% port.sol$pw
+      vol[k] = sqrt(as.vector(port.sol$pw %*% rcov %*% port.sol$pw)) #caclulate volatility
+      ret[k] = averet %*% port.sol$pw #calculate returns
+      names(port.sol$pw)<-ticker
+      port.sol$pw <- round(100*folio$pw)/100
+      wts[[k]] = port.sol$pw #store weights
     }
   }
-  return(list(vol = vol, ret = ret))
+  
+  return(list(vol = vol, ret = ret, weights = wts))
+
 }
---
+++ MVO_test.R
@@ -0,0 +1,11 @@
+source("C:\\Users\\David\\Documents\\Repos\\esc499-capstone-design\\MVO.R")
+ticker <- c("XIU.TO","RY.TO","F")
+from = as.Date('2013-10-01')
+to = Sys.Date()
+
+#download data from yahoo finance
+mylist <- lapply(ticker, function(x){
+  try(getSymbols(x, src = 'yahoo', from = from, to = to, auto.assign = FALSE))
+})
+
+folio <- MVO(ticker = ticker, mylist = mylist, wmax = 1, nports = 20, shorts = TRUE, rf = 0.01)
