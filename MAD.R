84dc4f623168ad31dc828c35893d23167f17f082
DBelvedere <davidjbelvedere@gmail.com>
2015-11-13 13:24:23 -0500

    MAD... with a twist. We should discuss as it straight up uses a package instead of our usual methods.

---
+++ MAD.R
@@ -0,0 +1,38 @@
+#load library fPortfolio
+require(fPortfolio)
+#packages.install("fPortfolio")
+#library(fPortfolio)
+
+
+MAD <- function(mylist, ticker){
+#use indicies LPP2005, see http://www.pictet.com/en/home/lpp_indices.html
+  for(i in 1:length(mylist)){
+    mylist[[i]] <- mylist[[i]][index(mylist[[1]])]
+  }
+  
+  r<-na.remove(do.call(merge,lapply(lapply(mylist,Ad),monthlyReturn))) #extract adjusted close annual returns
+  names(r) <- ticker
+
+  #create portfolio specification
+  frontierSpec  <- portfolioSpec();
+  
+  #optimization criteria - CVaR
+  setType(frontierSpec)  <- "MAD"
+  
+  #set optimization algorithm
+  setSolver(frontierSpec)  <- "solveRglpk.MAD"
+  
+  #set confidence level CVaR
+  #setAlpha(frontierSpec)  <- 0.05
+  
+  #number of portfolios in efficient frontier
+  setNFrontierPoints(frontierSpec)  <- 25
+  
+  #optimize, without shortselling
+  frontier <- portfolioFrontier(data = as.timeSeries(r), spec = frontierSpec, constraints="LongOnly");
+  
+  #build efficient frontier graph
+  tailoredFrontierPlot(object=frontier,mText="MAD Frontier (Long only)",risk="CVaR");
+  weightedReturnsPlot(frontier)
+  
+}
